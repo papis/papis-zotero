9d95d0dccf1bfa1ab30a1baa63fa73699924bcea
Alexandru Fikl <alexfikl@gmail.com>
2023-02-21 21:08:22 +0200

    ci: set up linting, support more versions, updates

---
+++ Makefile
@@ -0,0 +1,34 @@
+all: help
+
+help: 								## Show this help
+	@echo -e "Specify a command. The choices are:\n"
+	@grep -E '^[0-9a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[0;36m%-18s\033[m %s\n", $$1, $$2}'
+	@echo ""
+.PHONY: help
+
+tags:								## Generate ctags for main codebase
+	ctags -f tags \
+		--recurse=yes \
+		--tag-relative=yes \
+		--fields=+l \
+		--kinds-python=-i \
+		--language-force=python \
+		papis_zotero
+.PHONY: tags
+
+flake8:								## Run flake8 checks
+	python -m flake8 papis_zotero tests
+.PHONY: flake8
+
+mypy:								## Run (strict) mypy checks
+	python -m mypy papis_zotero tests
+.PHONY: mypy
+
+pytest:								## Run pytest test and doctests
+	python -m pytest -rswx --cov=papis_zotero -v -s tests
+.PHONY: pytest
+
+ci-install:							## Run pip and install dependencies on CI
+	python -m pip install --upgrade pip setuptools wheel
+	python -m pip install -e '.[develop]'
+.PHONY: ci-install
--- setup.py
+++ setup.py
@@ -1,45 +1,67 @@
-# -*- coding: utf-8 -*-
 from setuptools import setup
 
-with open('README.rst') as fd:
+with open("README.rst") as fd:
     long_description = fd.read()
 
 setup(
-    name='papis-zotero',
-    version='0.1.1',
-    author='Alejandro Gallo',
-    author_email='aamsgallo@gmail.com',
-    license='GPLv3',
-    url='https://github.com/papis/papis-zotero',
+    name="papis-zotero",
+    version="0.1.1",
+    author="Alejandro Gallo",
+    author_email="aamsgallo@gmail.com",
+    license="GPLv3",
+    url="https://github.com/papis/papis-zotero",
     install_requires=[
         "papis==0.13",
     ],
     classifiers=[
-        'Environment :: Console',
-        'Environment :: Console :: Curses',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: MacOS',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Utilities',
+        "Environment :: Console :: Curses",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft",
+        "Operating System :: OS Independent",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Utilities",
     ],
-    description='Interact with zotero using papis',
+    description="Interact with Zotero using papis",
     long_description=long_description,
     keywords=[
-        'papis', 'zotero', 'bibtex',
-        'management', 'cli', 'biliography'
+        "bibtex",
+        "biliography",
+        "cli",
+        "management",
+        "papis",
+        "zotero",
     ],
-    entry_points={
-        'papis.command': [
-            'zotero=papis_zotero:main'
+    extras_require={
+        "develop": [
+            "flake8",
+            "flake8-bugbear",
+            "flake8-quotes",
+            "mypy>=0.7",
+            "pep8-naming",
+            "pytest",
+            "pytest-cov",
+            "python-coveralls",
+            "types-PyYAML",
+            "types-tqdm",
         ]
     },
-    packages=['papis_zotero'],
-    platforms=['linux', 'osx'],
+    entry_points={
+        "papis.command": [
+            "zotero=papis_zotero:main"
+        ]
+    },
+    packages=["papis_zotero"],
+    platforms=["linux", "osx", "win32"],
 )
