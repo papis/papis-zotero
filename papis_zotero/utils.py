66762c1b78c1aedb84fcf77b514837fce68de368
Alexandru Fikl <alexfikl@gmail.com>
2023-06-03 11:30:38 +0300

    sql: use proper placeholders for query execution

--- papis_zotero/utils.py
+++ papis_zotero/utils.py
@@ -1,6 +1,5 @@
 import os
 import re
-from typing import Iterable
 
 
 def is_pdf(filepath: str) -> bool:
@@ -11,16 +10,3 @@ def is_pdf(filepath: str) -> bool:
         magic = fd.read(8)
 
     return re.match(r"%PDF-.\..", magic.decode()) is not None
-
-
-def to_sql_tuple(elements: Iterable[str]) -> str:
-    """Concatenate given strings to SQL tuple of strings."""
-
-    elements_tuple = "("
-    for element in elements:
-        if elements_tuple != "(":
-            elements_tuple += ","
-        elements_tuple += '"' + element + '"'
-    elements_tuple += ")"
-
-    return elements_tuple
