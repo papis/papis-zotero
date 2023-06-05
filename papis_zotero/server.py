19c0638e60fa17bd8ae6e65db15d9f5bbcaa504c
Alexandru Fikl <alexfikl@gmail.com>
2023-06-05 14:04:26 +0300

    server: update zotero-to-papis field conversions

--- papis_zotero/server.py
+++ papis_zotero/server.py
@@ -5,22 +5,15 @@ into papis.
 
 """
 
-import http.server
 import json
-import re
-import tempfile
-import urllib.request
-import urllib.error
-
-from typing import Any, Dict
+import http.server
+from typing import Any, Dict, List
 
 import papis.api
-import papis.config
 import papis.crossref
 import papis.document
+import papis.commands.add
 import papis.logging
-
-import papis_zotero.utils
 
 logger = papis.logging.get_logger(__name__)
 
@@ -28,44 +21,105 @@ ZOTERO_CONNECTOR_API_VERSION = 2
 ZOTERO_VERSION = "5.0.25"
 ZOTERO_PORT = 23119
 
+_k = papis.document.KeyConversionPair
+
+ZOTERO_IGNORED_FIELDS = frozenset({
+    "id", "attachments", "extra", "shortTitle", "accessDate",
+})
+
+ZOTERO_TO_PAPIS_CONVERSIONS = [
+    _k("creators", [{
+        "key": "author_list",
+        "action": lambda a: zotero_authors(a)
+        }]),
+    _k("tags", [{
+        "key": "tags",
+        "action": lambda t: "; ".join(tag["tag"] for tag in t)
+        }]),
+    _k("date", [
+        {"key": "year", "action": lambda d: int(d.split("-")[0])},
+        {"key": "month", "action": lambda d: int(d.split("-")[1])},
+        ]),
+    _k("archiveID", [
+        {"key": "eprint", "action": lambda a: a.split(":")[-1]}
+        ]),
+    _k("type", [
+        {"key": "type", "action": papis.bibtex.bibtex_type_converter.get}
+        ]),
+]
+
+
+def zotero_authors(creators: List[Dict[str, str]]) -> List[Dict[str, str]]:
+    authors = []
+    for creator in creators:
+        if creator["creatorType"] != "author":
+            continue
+
+        authors.append({
+            "given": creator["firstName"],
+            "family": creator["lastName"],
+        })
+
+    return authors
+
 
 def zotero_data_to_papis_data(item: Dict[str, Any]) -> Dict[str, Any]:
-    from papis_zotero.sql import ZOTERO_TO_PAPIS_FIELD_MAP
-    data = {}
-
-    # NOTE: these are handled elsewhere
     item.pop("id", None)
     item.pop("attachments", None)
 
-    # translate known zotero keys
-    for key in ZOTERO_TO_PAPIS_FIELD_MAP:
-        value = item.pop(key, None)
-        if value is not None:
-            data[ZOTERO_TO_PAPIS_FIELD_MAP[key]] = value
+    from papis_zotero.sql import ZOTERO_TO_PAPIS_FIELD_MAP
+    for foreign_key, key in ZOTERO_TO_PAPIS_FIELD_MAP.items():
+        if foreign_key in item:
+            item[key] = item.pop(foreign_key)
 
-    # check zotero tags
-    tags = item.pop("tags", None)
-    if isinstance(tags, list):
-        data["tags"] = " ".join(tags)
-
-    data.update(item)
+    item = papis.document.keyconversion_to_data(ZOTERO_TO_PAPIS_CONVERSIONS,
+                                                item,
+                                                keep_unknown_keys=True)
+    for key in ZOTERO_IGNORED_FIELDS:
+        if key in item:
+            del item[key]
 
     # try to get information from Crossref as well
-    doi = data.get("doi")
-    if doi is not None:
-        crossref_data = papis.crossref.doi_to_data(str(doi))
-        crossref_data.pop("title", None)
+    if "doi" in item:
+        try:
+            crossref_data = papis.crossref.doi_to_data(item["doi"])
+            crossref_data.pop("title", None)
+            logger.info("Updating document with data from Crossref.")
+        except ValueError:
+            crossref_data = {}
 
-        logger.info("Updating document with data from Crossref.")
-        data.update(crossref_data)
+        item.update(crossref_data)
 
-    return data
+    logger.info("Document metadata: %s", item)
+    return item
+
+
+def download_zotero_attachments(attachments: List[Dict[str, str]]) -> List[str]:
+    from papis.downloaders import download_document
+    from papis_zotero.sql import ZOTERO_INCLUDED_MIMETYPE_MAP
+    files = []
+
+    for attachment in attachments:
+        logger.info("Checking attachment: %s", attachment)
+
+        mime = str(attachment.get("mimeType"))
+        if mime not in ZOTERO_INCLUDED_MIMETYPE_MAP:
+            continue
+
+        url = attachment["url"]
+        extension = ZOTERO_INCLUDED_MIMETYPE_MAP[mime]
+        logger.info("Downloading file (%s): '%s'.", mime, url)
+
+        filename = download_document(url, expected_document_extension=extension)
+        if filename is not None:
+            files.append(filename)
+
+    return files
 
 
 class PapisRequestHandler(http.server.BaseHTTPRequestHandler):
-
     def log_message(self, fmt: str, *args: Any) -> None:
-        logger.info(fmt, args)
+        logger.info(fmt, *args)
 
     def set_zotero_headers(self) -> None:
         self.send_header("X-Zotero-Version", ZOTERO_VERSION)
@@ -77,114 +131,89 @@ class PapisRequestHandler(http.server.BaseHTTPRequestHandler):
         length = int(self.headers["content-length"])
         return self.rfile.read(length)
 
-    def pong(self, POST: bool = True) -> None:  # noqa: N803
-        # Pong must respond to ping on both GET and POST
-        # It must accepts application/json and text/plain
-        if not POST:  # GET
-            logger.debug("Received a GET request.")
-            self.send_response(200)
-            self.send_header("Content-Type", "text/html")
-            self.set_zotero_headers()
-            response = """\
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>Zotero Connector Server is Available</title>
-                </head>
-                <body>
-                    Zotero Connector Server is Available
-                </body>
-            </html>
-            """
-        else:  # POST
-            logger.debug("Received a POST request.")
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.set_zotero_headers()
-            response = json.dumps({"prefs": {"automaticSnapshots": True}})
+    def do_GET(self) -> None:  # noqa: N802
+        logger.info("Received GET request at '%s'", self.path)
+        if self.path == "/connector/ping":
+            self.handle_get_ping()
 
-        self.wfile.write(bytes(response, "utf8"))
+    def handle_get_ping(self) -> None:
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.set_zotero_headers()
+        response = """\
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Zotero Connector Server is Available</title>
+            </head>
+            <body>
+                Zotero Connector Server is Available
+            </body>
+        </html>
+        """
 
-    def papis_collection(self) -> None:
+        self.wfile.write(response.encode("utf-8"))
+
+    def do_POST(self) -> None:  # noqa: N802
+        logger.info("Received POST request at '%s'", self.path)
+        if self.path == "/connector/ping":
+            self.handle_post_ping()
+        elif self.path == "/connector/getSelectedCollection":
+            self.handle_post_collection()
+        elif self.path == "/connector/saveSnapshot":
+            self.handle_post_snapshot()
+        elif self.path == "/connector/saveItems":
+            self.handle_post_add()
+
+    def handle_post_ping(self) -> None:
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.set_zotero_headers()
+        response = json.dumps({"prefs": {"automaticSnapshots": True}})
+
+        self.wfile.write(response.encode("utf-8"))
+
+    def handle_post_collection(self) -> None:
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
         self.set_zotero_headers()
         papis_library = papis.api.get_lib_name()
+
         response = json.dumps({
             "libraryID": 1,
             "libraryName": papis_library,
-            # I'm not aware of a read-only papis mode
             "libraryEditable": True,
-            # collection-level parameters
             "editable": True,
-            # collection-level
             "id": None,
-            # collection if collection, else library
             "name": papis_library
         })
-        self.wfile.write(bytes(response, "utf8"))
 
-    def add(self) -> None:
+        self.wfile.write(response.encode("utf-8"))
+
+    def handle_post_add(self) -> None:
         logger.info("Adding paper from the Zotero Connector.")
         rawinput = self.read_input()
-        data = json.loads(rawinput.decode("utf8"))
+        data = json.loads(rawinput.decode("utf-8"))
 
+        logger.info("Response: %s", data)
         for item in data["items"]:
-            files = []
-            if item.get("attachments") and len(item.get("attachments")) > 0:
-                for attachment in item.get("attachments"):
-                    mime = str(attachment.get("mimeType"))
-                    logger.info("Checking attachment (mime %s).", mime)
-                    if re.match(r".*pdf.*", mime):
-                        url = attachment.get("url")
-                        logger.info("Downloading PDF: '%s'.", url)
-                        try:
-                            pdfbuffer = urllib.request.urlopen(url).read()
-                        except urllib.error.HTTPError:
-                            logger.error(
-                                "Error downloading PDF. You probably do not"
-                                "have the rights to access the journal.")
-                            continue
-
-                        pdfpath = tempfile.mktemp(suffix=".pdf")
-                        logger.info("Saving PDF: '%s'", pdfpath)
-
-                        with open(pdfpath, "wb+") as fd:
-                            fd.write(pdfbuffer)
-
-                        if papis_zotero.utils.is_pdf(pdfpath):
-                            files.append(pdfpath)
-                        else:
-                            logger.error(
-                                "File retrieved does not appear to be a PDF. "
-                                "Skipping!")
+            attachments = item.get("attachments", [])
+            if attachments:
+                files = download_zotero_attachments(attachments)
             else:
                 logger.info("Document has no attachments.")
+                files = []
 
             papis_item = zotero_data_to_papis_data(item)
             logger.info("Adding paper to papis.")
             papis.commands.add.run(files, data=papis_item)
 
-        self.send_response(201)  # Created
-        self.set_zotero_headers()
-        # return the JSON data back
-        self.wfile.write(rawinput)
-
-    def snapshot(self) -> None:
-        logger.error("Snapshot not implemented!")
         self.send_response(201)
         self.set_zotero_headers()
 
-    def do_POST(self) -> None:  # noqa: N802
-        if self.path == "/connector/ping":
-            self.pong()
-        elif self.path == "/connector/getSelectedCollection":
-            self.papis_collection()
-        elif self.path == "/connector/saveSnapshot":
-            self.snapshot()
-        elif self.path == "/connector/saveItems":
-            self.add()
+        self.wfile.write(rawinput)
 
-    def do_GET(self) -> None:  # noqa: N802
-        if self.path == "/connector/ping":
-            self.pong(POST=False)
+    def handle_post_snapshot(self) -> None:
+        logger.error("Snapshot not implemented!")
+        self.send_response(201)
+        self.set_zotero_headers()
