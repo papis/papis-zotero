b14bf45cf54549ed809107982d967008c16b46dd
Alexandru Fikl <alexfikl@gmail.com>
2024-11-16 15:44:19 +0200

    bibtex: format tags as a list

--- papis_zotero/bibtex.py
+++ papis_zotero/bibtex.py
@@ -1,5 +1,6 @@
 import os
-from typing import Optional
+import re
+from typing import Any, Dict, Optional
 
 import papis.bibtex
 import papis.commands.add
@@ -7,6 +8,8 @@ import papis.config
 import papis.logging
 
 logger = papis.logging.get_logger(__name__)
+
+RE_SEPARATOR = re.compile(r"\s*,\s*")
 
 
 def add_from_bibtex(bib_file: str,
@@ -18,28 +21,25 @@ def add_from_bibtex(bib_file: str,
     entries = papis.bibtex.bibtex_to_dict(bib_file)
     nentries = len(entries)
     for i, entry in enumerate(entries):
+        result: Dict[str, Any] = entry.copy()
+
+        # cleanup date
+        if "date" in result:
+            date = str(result.pop("date")).split("-")
+            result["year"] = int(date[0])
+            result["month"] = int(date[1])
+
         # cleanup tags
-        if "date" in entry:
-            date = str(entry.pop("date")).split("-")
-            entry["year"] = int(date[0])    # type: ignore[assignment]
-            entry["month"] = int(date[1])   # type: ignore[assignment]
+        if "keywords" in result:
+            result["tags"] = RE_SEPARATOR.split(result.pop("keywords"))
 
-        if "keywords" in entry:
-            entry["tags"] = entry.pop("keywords")
-
-        if "author" not in entry:
-            if "tags" in entry:
-                entry["tags"] = "{} unknown-author".format(entry["tags"])
-            else:
-                entry["tags"] = "unknown-author"
-
-        if "ref" in entry:
-            entry["ref"] = papis.bibtex.ref_cleanup(entry["ref"])
+        if "ref" in result:
+            result["ref"] = papis.bibtex.ref_cleanup(result["ref"])
         else:
-            entry["ref"] = papis.bibtex.create_reference(entry)
+            result["ref"] = papis.bibtex.create_reference(result)
 
         # get file
-        pdf_file = entry.pop("file", None)
+        pdf_file = result.pop("file", None)
         if pdf_file is not None:
             pdf_file = pdf_file.split(":")[1]
             pdf_file = os.path.join(*pdf_file.split("/"))
@@ -53,8 +53,8 @@ def add_from_bibtex(bib_file: str,
 
         # add to library
         logger.info("[%4d/%-4d] Exporting item with ref '%s'.",
-                    i, nentries, entry["ref"])
+                    i, nentries, result["ref"])
 
         papis.commands.add.run([pdf_file] if pdf_file is not None else [],
-                               data=entry,
+                               data=result,
                                link=link)
